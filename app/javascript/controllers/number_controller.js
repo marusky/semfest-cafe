055f882ec47fb9ac0ef1412265bc3f40ee15a5ed
marusky <adrianmarusakza@gmail.com>
2023-07-03 21:25:41 +0200

    Add sound (main)

--- app/javascript/controllers/number_controller.js
+++ app/javascript/controllers/number_controller.js
@@ -5,6 +5,10 @@ export default class extends Controller {
   connect() {
     this.element.classList.add('first-show-number')
 
+    const bell = document.getElementById('bell')
+    const bellAudio = new Audio(bell.src)
+    bellAudio.play()
+
     setTimeout(() => {
       this.element.classList.remove('first-show-number')
     }, 5000)
